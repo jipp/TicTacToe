70238f79f277e76b89b2d02242a013265c6323f0
jipp <wolfgang.keller@wobilix.de>
2016-04-17 19:51:38 +0200

    Initial Commit

---
+++ TicTacToe/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TicTacToe
+//
+//  Created by Wolfgang Keller on 17.04.16.
+//  Copyright © 2016 Wolfgang Keller. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
