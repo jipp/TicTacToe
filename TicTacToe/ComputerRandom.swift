a496d0cc6a4ba81613273f6b6ea8288a398abe60
jipp <wolfgang.keller@wobilix.de>
2016-04-27 22:32:00 +0200

    bugfixes

---
+++ TicTacToe/ComputerRandom.swift
@@ -0,0 +1,13 @@
+//
+//  ComputerRandom.swift
+//  TicTacToe
+//
+//  Created by Wolfgang Keller on 27.04.16.
+//  Copyright © 2016 Wolfgang Keller. All rights reserved.
+//
+
+import Foundation
+
+class ComputerRandom: PlayerClass {
+
+}
