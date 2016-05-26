534d1f2c3e02e33c8c722cfc8dc2f00b4a1fb5fc
jipp <wolfgang.keller@wobilix.de>
2016-05-26 11:43:19 +0200

    last changes - depth set to 5 (seems to be sufficient)

--- TicTacToe/ComputerMinMax.swift
+++ TicTacToe/ComputerMinMax.swift
@@ -9,16 +9,12 @@
 import Foundation
 
 class ComputerMinMax: PlayerClass {
-    var count: Int = 0
     
     override func getMove(field: Field) -> (x: Int, y: Int) {
-        count = 0
-        
         print("\(figure) turn")
         
-        (x, y) = maximizing(field, depth: 10)
+        (x, y) = maximizing(field, depth: 5)
 
-        print(" (\(count))")
         return (x, y)
     }
     
@@ -26,15 +22,12 @@ class ComputerMinMax: PlayerClass {
         var maxValue: Int = Int.min
         var value: Int
         
-        count += 1
-        
         for i in 0...field.getLines()-1 {
             for j in 0...field.getLines()-1 {
                 if (field.content[i][j] == Figure.empty) {
                     field.content[i][j] = figure
                     value = minimizing(field, depth: depth-1)
                     field.content[i][j] = Figure.empty
-                    print(i, j, value)
                     if (value > maxValue) {
                         maxValue = value
                         x = i
@@ -52,10 +45,8 @@ class ComputerMinMax: PlayerClass {
         var maxValue: Int = Int.min
         var value: Int
         
-        count += 1
-        
         if (field.won()) {
-            return  -1-depth
+            return  -1 - depth
         }
         if (field.draw() || depth == 0) {
             return 0
@@ -81,10 +72,8 @@ class ComputerMinMax: PlayerClass {
         var minValue: Int = Int.max
         var value: Int
         
-        count += 1
-        
         if (field.won()) {
-            return 1+depth
+            return 1 + depth
         }
         if (field.draw() || depth == 0) {
             return 0
