0ef4b4447caa3816606468a0ce630c93080b3424
jipp <wolfgang.keller@wobilix.de>
2016-05-27 07:20:06 +0200

    finally the correct aalpha/beta

--- TicTacToe/ComputerMinMax.swift
+++ TicTacToe/ComputerMinMax.swift
@@ -9,41 +9,54 @@
 import Foundation
 
 class ComputerMinMax: PlayerClass {
+    var count: Int = 0
     
     override func getMove(field: Field) -> (x: Int, y: Int) {
+        count = 0
+        
         print("\(figure) turn")
         
-        (x, y) = maximizing(field, depth: 5)
+        (x, y) = maximizing(field, depth: 10, alpha: Int.min, beta: Int.max)
+        
+        print(count)
 
         return (x, y)
     }
     
-    func maximizing(field: Field, depth: Int) -> (Int, Int) {
-        var maxValue: Int = Int.min
+    func maximizing(field: Field, depth: Int, alpha: Int, beta: Int) -> (Int, Int) {
+        var maxValue: Int = alpha
         var value: Int
         
-        for i in 0...field.getLines()-1 {
+        count += 1
+        
+        move: for i in 0...field.getLines()-1 {
             for j in 0...field.getLines()-1 {
                 if (field.content[i][j] == Figure.empty) {
                     field.content[i][j] = figure
-                    value = minimizing(field, depth: depth-1)
+                    value = minimizing(field, depth: depth-1, alpha: maxValue, beta: beta)
                     field.content[i][j] = Figure.empty
+                    print(i, j, value)
                     if (value > maxValue) {
                         maxValue = value
                         x = i
                         y = j
+                        if (maxValue >= beta) {
+                            break move
+                        }
                     }
                 }
             }
         }
-        
+        print("result", x, y, maxValue)
         return (x,y)
     }
 
     
-    func maximizing(field: Field, depth: Int) -> Int {
+    func maximizing(field: Field, depth: Int, alpha: Int, beta: Int) -> Int {
         var maxValue: Int = Int.min
         var value: Int
+        
+        count += 1
         
         if (field.won()) {
             return  -1 - depth
@@ -52,14 +65,17 @@ class ComputerMinMax: PlayerClass {
             return 0
         }
         
-        for i in 0...field.getLines()-1 {
+        move: for i in 0...field.getLines()-1 {
             for j in 0...field.getLines()-1 {
                 if (field.content[i][j] == Figure.empty) {
                     field.content[i][j] = figure
-                    value = minimizing(field, depth: depth-1)
+                    value = minimizing(field, depth: depth-1, alpha: maxValue, beta: beta)
                     field.content[i][j] = Figure.empty
                     if (value > maxValue) {
                         maxValue = value
+                        if (maxValue >= beta) {
+                            break move
+                        }
                     }
                 }
             }
@@ -68,9 +84,11 @@ class ComputerMinMax: PlayerClass {
         return maxValue
     }
 
-    func minimizing(field: Field, depth: Int) -> Int {
+    func minimizing(field: Field, depth: Int, alpha: Int, beta: Int) -> Int {
         var minValue: Int = Int.max
         var value: Int
+        
+        count += 1
         
         if (field.won()) {
             return 1 + depth
@@ -79,14 +97,17 @@ class ComputerMinMax: PlayerClass {
             return 0
         }
         
-        for i in 0...field.getLines()-1 {
+        move: for i in 0...field.getLines()-1 {
             for j in 0...field.getLines()-1 {
                 if (field.content[i][j] == Figure.empty) {
                     field.content[i][j] = getOpponent()
-                    value = maximizing(field, depth: depth-1)
+                    value = maximizing(field, depth: depth-1, alpha: alpha, beta: minValue)
                     field.content[i][j] = Figure.empty
                     if (value < minValue) {
                         minValue = value
+                        if (minValue <= alpha) {
+                            break move
+                        }
                     }
                 }
             }
